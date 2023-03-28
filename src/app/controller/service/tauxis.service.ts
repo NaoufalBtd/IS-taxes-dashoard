db9267bb4507137f3603188f477fbf6228bcfa4a
hibat allah <hibaallah703@gmail.com>
2023-03-28 13:26:17 +0000

    model et service avec methode

--- src/app/controller/service/tauxis.service.ts
+++ src/app/controller/service/tauxis.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import {Tauxir} from "../model/tauxir.model";
 import {Tauxis} from "../model/tauxis.model";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Employe} from "../model/employe.model";
 
 @Injectable({
   providedIn: 'root'
